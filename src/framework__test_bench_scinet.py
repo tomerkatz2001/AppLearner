30b33586f491369f594b25d55453d05792171c50
Alon Kitin <alon.kitin@galileo2.csf.technion.ac.il>
2023-05-29 18:55:57 +0300

    extra space

--- src/framework__test_bench_scinet.py
+++ src/framework__test_bench_scinet.py
@@ -4,7 +4,6 @@
 ***********************************************************************************************************************
 """
 import os
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
